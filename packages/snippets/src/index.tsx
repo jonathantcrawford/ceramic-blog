76e72a65b9d0b3ca35451273d46ca11cf7fcab65
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-03-17 17:48:29 -0500

    setup tailwind for snippet library

--- packages/snippets/src/index.tsx
+++ packages/snippets/src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
-
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className='py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>Example Component: {text}</div>
+  return (
+    <div className='mx-auto max-w-sm space-y-2 rounded-xl bg-blue py-8 px-8 shadow-lg sm:flex sm:items-center sm:space-y-0 sm:space-x-6 sm:py-4'>
+      Example Component: {text}
+    </div>
+  )
 }
