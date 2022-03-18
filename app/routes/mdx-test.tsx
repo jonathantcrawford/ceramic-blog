d81b28ad3ec4d2203c930505b01238e13669b910
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-03-17 19:51:12 -0500

    pf

--- app/routes/mdx-test.tsx
+++ app/routes/mdx-test.tsx
@@ -1,44 +1,30 @@
-
-import type { LoaderFunction, LinksFunction } from "remix"
-import {json, useLoaderData} from "remix"
+import type { LoaderFunction, LinksFunction } from "remix";
+import { json, useLoaderData } from "remix";
 
 import { compileMDX } from "~/compile-mdx.server";
 
-import  {useMemo, useState, useEffect, Fragment } from "react";
+import { useMemo, useState, useEffect, Fragment } from "react";
 
+import { getMDXComponent } from "mdx-bundler/client";
 
-
-import {getMDXComponent} from 'mdx-bundler/client'
-
-import styles from '@ceramic-blog/snippets/dist/main.css';
+import styles from "@ceramic-blog/snippets/dist/main.css";
 
 export const links: LinksFunction = () => {
-  return [
-
-    { rel: "stylesheet", href: styles}
-  ];
+  return [{ rel: "stylesheet", href: styles }];
 };
 
 export const loader: LoaderFunction = async () => {
-
-    const {code} = await compileMDX();
-    return json({code});
-}
+  const { code } = await compileMDX();
+  return json({ code });
+};
 
 export default function MDXTest() {
+  const { code } = useLoaderData();
 
-    const { code } = useLoaderData();
-
-    const Component = useMemo(
-        () => getMDXComponent(code),
-        [code],
-      )
-      return (
-        <main>
-          <Component />
-        </main>
-      )
-
-
-    
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+  return (
+    <main>
+      <Component />
+    </main>
+  );
 }
