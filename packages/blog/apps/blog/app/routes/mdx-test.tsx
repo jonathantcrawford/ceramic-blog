6c3246385025ed71b10f57e4e207aed4038c83ab
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-03-18 03:10:48 -0500

    move to monorepo struct

---
+++ packages/blog/apps/blog/app/routes/mdx-test.tsx
@@ -0,0 +1,30 @@
+import type { LoaderFunction, LinksFunction } from "remix";
+import { json, useLoaderData } from "remix";
+
+import { compileMDX } from "src/blog/app/compile-mdx.server";
+
+import { useMemo } from "react";
+
+import { getMDXComponent } from "mdx-bundler/client";
+
+import styles from "@jontcrawford/snippets/dist/main.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export const loader: LoaderFunction = async () => {
+  const { code } = await compileMDX();
+  return json({ code });
+};
+
+export default function MDXTest() {
+  const { code } = useLoaderData();
+
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+  return (
+    <main>
+      <Component />
+    </main>
+  );
+}
