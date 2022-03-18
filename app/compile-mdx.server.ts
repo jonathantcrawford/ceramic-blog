e0dda95a965b77eb0e932b9ed3c740bbad31c4da
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-03-18 00:23:06 -0500

    add binary lookup

--- app/compile-mdx.server.ts
+++ app/compile-mdx.server.ts
@@ -1,5 +1,24 @@
 import { bundleMDX } from "mdx-bundler";
 
+import path from 'path'
+
+if (process.platform === 'win32') {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'esbuild.exe',
+  )
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'bin',
+    'esbuild',
+  )
+}
+
 export const compileMDX = async () => {
   const mdxSource = `
 ---
