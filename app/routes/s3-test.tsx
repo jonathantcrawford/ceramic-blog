321e545f5a1ae4cca2efba574d73e37cbca92b3d
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-03-31 14:59:40 -0500

    add a test route

--- app/routes/s3-test.tsx
+++ app/routes/s3-test.tsx
@@ -7,31 +7,60 @@ import {
   Form
 } from 'remix';
 import { useEffect } from "react";
-//import { s3UploadHandler } from '~/s3-upload.server';
+import { updateBlogPostImages, getBlogPostById } from "~/models/blog_post.server";
+import type { PutObjectCommandInput } from '@aws-sdk/client-s3';
+import { deleteObjectsFromS3, uploadToS3 } from '~/s3-upload.server';
+import cuid from 'cuid';
 
+export const action: ActionFunction = async ({ request, context, params }) => {
+  const uploadHandler: UploadHandler = async ({ name, filename, mimetype, encoding, stream }) => {
+    if (name !== "imageFile") {
+      stream.resume();
+      return;
+    }
+    const key = `${process.env.S3_ENV_PREFIX}/test`;
 
-// export const action: ActionFunction = async ({ request, context, params }) => {
-//   console.log(request.headers)
-//   console.log(params)
-//   let formData = await parseMultipartFormData(request, s3UploadHandler);
+    const params: PutObjectCommandInput = {
+      Bucket: process.env.S3_BUCKET ?? "",
+      Key: key,
+      Body: stream,
+      ContentType: mimetype,
+      ContentEncoding: encoding,
+      Metadata: {
+        filename: filename,
+      },
+    };
 
-//   // let formData;
-//   // try {
-//   //   formData = await parseMultipartFormData(request, s3UploadHandler);
-//   // } catch (error) {
-//   //   console.log('Caught: ', error);
-//   //   return json({ error }, { status: 400 });
-//   // }
+  
+    try {
+      
+      await uploadToS3({params});
 
-//   let cover = JSON.parse(formData.get('cover') as string);
+    } catch (e) {
+      console.log(e);
+    }
+  
+    return JSON.stringify({ filename, key });
+  }
+  let formData = await parseMultipartFormData(request, uploadHandler);
 
-//   return json(
-//     {
-//       fields: { cover },
-//     },
-//     { status: cover.error ? 400 : 200 }
-//   );
-// };
+  // let formData;
+  // try {
+  //   formData = await parseMultipartFormData(request, s3UploadHandler);
+  // } catch (error) {
+  //   console.log('Caught: ', error);
+  //   return json({ error }, { status: 400 });
+  // }
+
+  let cover = JSON.parse(formData.get('imageFile') as string);
+
+  return json(
+    {
+      fields: { cover },
+    },
+    { status: cover.error ? 400 : 200 }
+  );
+};
 export default function S3Test() {
 
   const actionData = useActionData();
@@ -44,7 +73,7 @@ export default function S3Test() {
     return (
       <>
         <Form method="post" encType="multipart/form-data">
-            <input type="file" name="cover" accept='image/png'/>
+            <input type="file" name="imageFile" accept='image/png'/>
             <button type="submit" className="btn">submit</button>
         </Form>
       </>
