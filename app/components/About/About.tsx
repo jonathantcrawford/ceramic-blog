3c1789fb95fb0bf807aa07509a5d2c76ac6a7ad6
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-05-20 21:12:27 -0500

    try removing prefetching

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -29,7 +29,7 @@ export const About = () => {
       </div>
       <div className={divClasses}>
         Feel free to check out my{" "}
-        <NavLink prefetch="render" to="/blog"  className={navLinkClasses}>
+        <NavLink prefetch="none" to="/blog"  className={navLinkClasses}>
           blog
         </NavLink>
         .
