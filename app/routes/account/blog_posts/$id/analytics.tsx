7a832b8db4662f2c0a88272458bcf7d93343f868
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-05-31 20:54:51 -0500

    change analytics to on client load

--- app/routes/account/blog_posts/$id/analytics.tsx
+++ app/routes/account/blog_posts/$id/analytics.tsx
@@ -12,14 +12,14 @@ export const loader: LoaderFunction = async ({request, params}: any) => {
     
     const blogPost = await getBlogPostById({id});
     const analytics = await getBlogPostAnalytics({blogPostId: blogPost?.id });
-    console.log(analytics)
     const url = new URL(request.url);
-    return {analytics, blogPost, host: url.host, protocol: url.protocol}
+    const domain = process.env.NODE_ENV === "development" ? url.host : process.env.DOMAIN;
+    return {analytics, blogPost, domain, protocol: url.protocol}
 }
 
 export default function BlogPostAnalytics() {
 
-    const {blogPost, host, protocol, analytics} = useLoaderData();
+    const {blogPost, domain, protocol, analytics} = useLoaderData();
 
     const linkedInViews = analytics.filter((analytic: any) => analytic.utm_source === 'linkedin');
     const twitterViews = analytics.filter((analytic: any) => analytic.utm_source === 'twitter');
@@ -30,20 +30,20 @@ export default function BlogPostAnalytics() {
         <div className="flex flex-col">
             <div className="flex flex-col">
                 <span className="font-saygon text-yellow-100 text-base">LinkedIn Views: {linkedInViews.length}</span>
-                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${host}/blog/${blogPost?.slug}?utm_source=linkedin`}</code>
+                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${domain}/blog/${blogPost?.slug}?utm_source=linkedin`}</code>
             </div>
             <div className="flex flex-col">
                 <span className="font-saygon text-yellow-100 text-base">Twitter Views: {twitterViews.length}</span>
-                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${host}/blog/${blogPost?.slug}?utm_source=twitter`}</code>
+                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${domain}/blog/${blogPost?.slug}?utm_source=twitter`}</code>
             </div>
             <div className="flex flex-col">
                 <span className="font-saygon text-yellow-100 text-base">Slack Views: {slackViews.length}</span>
-                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${host}/blog/${blogPost?.slug}?utm_source=slack`}</code>
+                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${domain}/blog/${blogPost?.slug}?utm_source=slack`}</code>
             </div>
 
             <div className="flex flex-col">
                 <span className="font-saygon text-yellow-100 text-base">Direct Views: {directViews.length}</span>
-                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${host}/blog/${blogPost?.slug}`}</code>
+                <code className="font-mono text-pink-200 bg-gray-100">{`${protocol}//${domain}/blog/${blogPost?.slug}`}</code>
             </div>
         </div>
     )
